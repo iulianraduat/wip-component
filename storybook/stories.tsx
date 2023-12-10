249b0eee47b18cbca9ae2aba3bec248ca410b6ec
Iulian Radu <iulian.radu@gmx.at>
2023-12-10 21:09:13 +0100

    Update packages and storybook

--- storybook/stories.tsx
+++ storybook/stories.tsx
@@ -1,5 +1,5 @@
+import { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { _WIPComponent } from '../src/_WIPComponent';
 
 const style: React.CSSProperties = {
@@ -13,14 +13,14 @@ const SeeMe: React.FC = () => (
   </div>
 );
 
-export default {
+const meta: Meta<typeof _WIPComponent> = {
   title: 'WIP',
   component: _WIPComponent,
-} as ComponentMeta<typeof _WIPComponent>;
+} as Meta<typeof _WIPComponent>;
+export default meta;
+type Story = StoryObj<typeof _WIPComponent>;
 
-export const HideTheComponentInProduction: ComponentStory<
-  typeof _WIPComponent
-> = () => (
+export const HideTheComponentInProduction = () => (
   <div style={style}>
     <div>You should not see a text below!</div>
     <_WIPComponent env={'production'}>
@@ -29,9 +29,7 @@ export const HideTheComponentInProduction: ComponentStory<
   </div>
 );
 
-export const ShowsTheComponentInDevelopment: ComponentStory<
-  typeof _WIPComponent
-> = () => (
+export const ShowsTheComponentInDevelopment = () => (
   <div style={style}>
     <div>You should see a text below!</div>
     <_WIPComponent env={'development'}>
@@ -40,9 +38,7 @@ export const ShowsTheComponentInDevelopment: ComponentStory<
   </div>
 );
 
-export const HideTheComponentInOtherEnvironments: ComponentStory<
-  typeof _WIPComponent
-> = () => (
+export const HideTheComponentInOtherEnvironments = () => (
   <div style={style}>
     <div>You should not see a text below!</div>
     <_WIPComponent env={'staging'}>
@@ -51,10 +47,9 @@ export const HideTheComponentInOtherEnvironments: ComponentStory<
   </div>
 );
 
-export const AcceptsANullChildren: ComponentStory<typeof _WIPComponent> =
-  () => (
-    <div style={style}>
-      <div>You should not see a text below (even in development)!</div>
-      <_WIPComponent env={'development'} children={null} />
-    </div>
-  );
+export const AcceptsANullChildren = () => (
+  <div style={style}>
+    <div>You should not see a text below (even in development)!</div>
+    <_WIPComponent env={'development'} children={null} />
+  </div>
+);
